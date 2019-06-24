5697207232fceeff5f44e583fa2cb3879c3478f7
Chidi Nkwocha <profchydon@gmail.com>
2019-06-24 15:45:42 +0100

    Added video icon

--- resources/views/pages/escort.blade.php
+++ resources/views/pages/escort.blade.php
@@ -156,7 +156,7 @@
                               <!-- Hidden video div -->
                               <!-- data-src should not be provided when you use html5 videos -->
                                 <li data-poster="{{ asset('img/e.jpg') }}" data-sub-html="{{ $video }}" data-html="#{{ $videoCount }}" >
-                                  <video id="video-js-small" class="sm-video-object sm-html5 video-js vjs-default-skin" controls preload="none" playsinline autoplay muted loop>
+                                  <video id="video-js-small" class="sm-video-object sm-html5 video-js vjs-default-skin" controls preload="none" playsinline muted loop>
                                       <source src="{{ asset($this_video) }}" type="video/mp4">
                                        Your browser does not support HTML5 video.
                                   </video>
